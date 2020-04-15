c7283ad58053894bcfa4539e3aaa1e37a38f9dce
“dziekan30” <dziekan30@gmail.com>
2020-04-15 14:47:43 -0500

    first scrach

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- header -->
+    <div class="jumbotron header">
+      <h1 class="display-2 header">Welcome to Irek Mar Salon and Spa!</h1>
+    </div>
+    
+
+    <!-- footer -->
+
+    <div class="card text-center footer">
+      <div class="card-body ">
+        <h5 class="card-title">Follow us</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <i class="fab fa-facebook"></i>
+        <font-awesome-icon icon="user-secret" />
+        <font-awesome-icon icon="coffee"></font-awesome-icon>
+        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {};
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
