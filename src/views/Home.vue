ae691850a04b5b83f6f80bb3980a995a20fd030b
“dziekan30” <dziekan30@gmail.com>
2020-04-18 01:22:55 -0500

    added icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,8 +17,22 @@
     <!-- body -->
 
     <div class="container">
-      <h1>About Us</h1>
+      <h1 class="text margin">About Us</h1>
       <div class="card mb-3" style="max-width: 940px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 " style="max-width: 940px;">
         <div class="row no-gutters">
           <div class="col-md-4">
             <img src="" class="card-img" alt="...">
@@ -46,21 +60,7 @@
         </div>
       </div>
 
-      <div class="card mb-3" style="max-width: 940px;">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img src="" class="card-img" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-     <div class="row">
+     <div class="row margin">
        <div class="col-sm-6">
          <div class="card">
            <div class="card-body">
@@ -84,13 +84,24 @@
 
     </div>
      
+     <template>
+       <div>
+         <div class="google-map" ref="googleMap"></div>
+         <template v-if="Boolean(this.google) && Boolean(this.map)">
+           <slot
+             :google="google"
+             :map="map"
+           />
+         </template>
+       </div>
+     </template>
 
 
     <!-- footer -->
 
     <div class="card text-center footer">
       <div class="card-body ">
-        <h1 class="card-title">Follow us</h1>
+        <h1 class=" text card-title">Follow us</h1>
         <div class="row">
           <div class="container col-sm-4 ">
             <img src="https://scontent-ort2-2.xx.fbcdn.net/v/t31.0-8/10714396_854218474610980_5548625635518804257_o.jpg?_nc_cat=103&_nc_sid=85a577&_nc_oc=AQm6obXbU9ULz5JUPyyQ7aTUm5JnWOhDcpear-rgvmVtWboqXb8grCcqq--7ss_D6aMdzwax6aPuLcXQVwE9xV_w&_nc_ht=scontent-ort2-2.xx&oh=21fd62f301c8f09d891be1130c7c7f86&oe=5EBEE864" class="rounded mx-auto d-block footer-logo" alt="...">
